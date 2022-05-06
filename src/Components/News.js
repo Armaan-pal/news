515ff264f7b4a633e6bee9c956d87618a0274965
Armaan Pal <armaanpal25@gmail.com>
2022-05-06 14:17:16 -0400

    first commit fixed version

--- src/Components/News.js
+++ src/Components/News.js
@@ -20,6 +20,7 @@ export class News extends Component {
         let url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=c8a129b81b9c483fbcce9ff1cde549cc&page=1&pageSize=18";
         let data = await fetch(url);
         let parsedData = await data.json()
+        console.log("parsedData", parsedData)
         this.setState({ articles: parsedData.articles })
     }
 
@@ -55,7 +56,7 @@ export class News extends Component {
                 <h2> TOP HEADLINES </h2>
                 <div className="row">
 
-                    {this.state.articles.map((element) => {
+                    {this.state.articles?.map((element) => {
                         return <div className="col-md-4" key={element.url}>
                             <NewsIteam title={element?.title?.slice(0, 40)} description={element?.description?.slice(0, 88)} imageUrl={element?.urlToImage || No_image} newsUrl={element.url} />
                         </div>
@@ -66,8 +67,8 @@ export class News extends Component {
 
                 <div className='container- d-flex justify-content-between'>
 
-                    <button disabled={this.state.page <= 1} type="button" class="btn btn-dark" onClick={this.handelPrevClick}>Previous &larr;</button>
-                    <button type="button" class="btn btn-dark" onClick={this.handelNextClick}>&rarr; Next</button>
+                    <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handelPrevClick}>Previous &larr;</button>
+                    <button type="button" className="btn btn-dark" onClick={this.handelNextClick}>&rarr; Next</button>
 
                 </div>
 
